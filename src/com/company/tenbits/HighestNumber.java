6f787a166b244692b265b21f8c900768313e0112
hari-p4 <51733509+hari-p4@users.noreply.github.com>
2020-01-07 14:18:11 -0600

    Initial Commit

---
+++ src/com/company/tenbits/HighestNumber.java
@@ -0,0 +1,12 @@
+package com.company.tenbits;
+
+public class HighestNumber {
+
+        public static void
+        FindLargeNumber(int Large, int Small){
+            int result = (((Large + Small) + (Large - Small)) / 2);
+           System.out.println(result);
+        }
+    }
+
+
---
+++ src/com/company/tenbits/Main.java
@@ -0,0 +1,11 @@
+package com.company.tenbits;
+
+public class Main {
+    public static void main(String[] args) {
+       // HighestNumber.FindLargeNumber(5,2);
+
+        System.out.println( + WordOfNumber.convert(123));
+        System.out.println( + WordOfNumber.convert(1995));
+        System.out.println( + WordOfNumber.convert(1996));
+    }
+}
