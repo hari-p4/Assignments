b55d1fcd877d490d5b740b410df3c1bed90ade3a
hari-p4 <51733509+hari-p4@users.noreply.github.com>
2020-01-07 19:15:13 -0600

    All bugs fixed with AND funtionality added

--- src/com/company/tenbits/Main.java
+++ src/com/company/tenbits/Main.java
@@ -2,10 +2,10 @@ package com.company.tenbits;
 
 public class Main {
     public static void main(String[] args) {
-         // HighestNumber.FindLargeNumber(5,2);
+        // HighestNumber.FindLargeNumber(5,2);
 
-        System.out.println( WordOfNumber.convert(125));
-        System.out.println(  WordOfNumber.convert(2095));
-        System.out.println(  WordOfNumber.convert(1996));
+        System.out.println(WordOfNumber.convert(100));
+        System.out.println(WordOfNumber.convert(1965));
+        System.out.println(WordOfNumber.convert(1996));
     }
 }
