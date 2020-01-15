7b24bb5d9c3a9c81fa7ea0dd18120de33acce528
hari-p4 <51733509+hari-p4@users.noreply.github.com>
2020-01-14 20:38:43 -0600

    Last code file

---
+++ src/tenbitsAssignment5/SortNames.java
@@ -0,0 +1,10 @@
+package com.company.tenbitsAssignment5;
+
+import java.util.Arrays;
+
+public class SortNames {
+    public static void sortFromArray(String[] names){
+        Arrays.sort(names);
+        System.out.println("Array after sorting names "+ Arrays.toString(names));
+    }
+}
