c25a7de924ed9f073e5d9e0da5e641187124b521
hari-p4 <51733509+hari-p4@users.noreply.github.com>
2020-01-16 21:06:12 -0600

    bugs fixed

--- src/tenbitsAssignment6/RepeatedWord.java
+++ src/tenbitsAssignment6/RepeatedWord.java
@@ -1,11 +1,43 @@
 package com.company.tenbitsAssignment6;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RepeatedWord {
 
-    public static void getNumberofWords(String  find){
-        String str = "Create 2 classes parent child between vehicanle is base calas  child is sedan suv and truck and two of thos 3 child classesshould not be able to initiate a child class";
+    public static void getNumberofWords(String input){
+        String str = input;
         int stringCount = 0;
             String[] StringArray = str.split(" ");
+        Map<String, Integer> checkVisitedWord = new HashMap<String, Integer>();
+
+        for(String s : StringArray){
+                if(checkVisitedWord.containsKey(s)){
+                    int mostVisited = checkVisitedWord.get(s);
+                    mostVisited++;
+                    checkVisitedWord.put(s, mostVisited);
+                }
+                else {
+                    checkVisitedWord.put(s, 1);
+                }
+        }
+        int highestCount = 0;
+        String res = "-1";
+        for(Map.Entry<String,Integer> val : checkVisitedWord.entrySet()){
+            if(highestCount < val.getValue()){
+                res = val.getKey();
+                highestCount = val.getValue();
+
+            }
+        }
+        System.out.println(res + "  is the most Repeated Word in the given Paragraph");
+
+
+    }
+}
+
+
+/*
 
             for(String s : StringArray){
                    if(s.equals(find)) {
@@ -13,6 +45,4 @@ public class RepeatedWord {
                    }
             }
             System.out.println("String " + find+" was found " + stringCount + " times.");
-
-    }
-}
+*/
